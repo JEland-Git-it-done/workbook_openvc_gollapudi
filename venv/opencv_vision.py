7e8ef1d12d06df54c0847db2610d742ffa107a25
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2020-03-27 10:57:22 +0100

    Created first method with own picture
    As of opencv 4.2, a file id is required in the "imwrite" method

--- venv/opencv_vision.py
+++ venv/opencv_vision.py
@@ -1,4 +1,26 @@
-import numpy; import pandas
+import numpy as np; import pandas as pd
 import cv2 as opencv
+import urllib
 
 print(opencv.__version__)
+
+def test_numpy_matrix():
+    newlst = [1,2,3]
+    newarr = np.array(newlst)
+    g = np.zeros(shape=(3,2))
+    g = np.ones((2,4))
+    print(g)
+
+def show_panda():
+    src = 'C:/Users/jedel/Pictures/Saved Pictures/1280px-Panda_Cub_from_Wolong,_Sichuan,_China.png'
+    panda_img = opencv.imread(src)
+    print(panda_img)
+    panda_gray_img = opencv.cvtColor(panda_img,
+                     opencv.COLOR_BGR2GRAY)
+    opencv.imshow("Gray panda", panda_gray_img)
+    opencv.imshow("Color panda", panda_gray_img)
+    opencv.imwrite("gray_panda.png", panda_gray_img)
+    opencv.waitKey(0)
+    opencv.destroyAllWindows()
+
+show_panda()
